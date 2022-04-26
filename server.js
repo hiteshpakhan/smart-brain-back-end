baeccd84df94999957a045470a1813be6b1c57bc
Hitesh Pakhan <hitesh5pakhan@gmail.com>
2022-04-26 16:14:58 +0530

    a

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors())
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 app.get('/', (req, res)=> {
-  res.send(database.users);
+  res.send("it is working");
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
